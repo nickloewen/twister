41ff2dd3a71aa508a2463b2b7fc8eb89e441145c
Nick Loewen <nick@tidepool.ca>
2013-04-08 12:33:39 -0700

    tidied up script, plus some other minor changes

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-function bodyPart(name) {
-	this.name = name;
-	this.color = "unset"; 
+function BodyPart(name) {
+    this.name = name;
+    this.color = "unset";
 }
 
-var lFoot = new bodyPart("Left foot"),
-	rFoot = new bodyPart("Right foot"),
-	lHand = new bodyPart("Left hand"),
-	rHand = new bodyPart("Right hand");
+var lFoot = new BodyPart("Left foot"),
+    rFoot = new BodyPart("Right foot"),
+    lHand = new BodyPart("Left hand"),
+    rHand = new BodyPart("Right hand");
 
 var bodyParts = [lFoot, rFoot, lHand, rHand];
 var colors = ["red", "yellow", "green", "blue"];
 
-function nextAction() {
-	var nextBodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)],
-		nextColor = colors[Math.floor(Math.random() * colors.length)];
-
-	moveYourBody(nextBodyPart, nextColor);
+function moveYourBody(bodyPart, color) {
+    if (bodyPart.color === color) {
+        // already on that color, so try again
+        nextAction();
+    } else {
+        // move it
+        bodyPart.color = color;
+        // write it on the page
+        document.getElementById("bodyPart").innerHTML = bodyPart.name;
+        document.getElementById("color").innerHTML = color;
+    }
 }
 
-function moveYourBody(bodyPart, color) {
-	if (bodyPart.color == color) {
-		// already on that color, so try again
-		nextAction();
-	} else {
-		// move it
-		bodyPart.color = color;
-		// write it on the page
-		document.getElementById("bodyPart").innerHTML = bodyPart.name;
-		document.getElementById("color").innerHTML = color;
-	}
+function nextAction() {
+    var nextBodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)],
+        nextColor = colors[Math.floor(Math.random() * colors.length)];
+
+    moveYourBody(nextBodyPart, nextColor);
 }

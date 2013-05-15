d366fbbb3f95312dce1971d9931674df82cee6b3
Nick Loewen <nick@tidepool.ca>
2013-05-14 22:59:59 -0700

    Improve syntax in js, css, and html, and add a todo section to README

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 twister
 =======
 
-A small website to serve as a “spinner” when playing Twister.
+A small website to serve as a "spinner" when playing Twister.
 
 *Everything in this repository is released into the public
 domain.*
+
+todo
+----
+* potentially improve styling, especially on mobile browsers (font sizes and
+  positioning can be a little strange, for example)
--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 function BodyPart(name) {
-    this.name = name;
-    this.color = "unset";
+	/* An object representing a body part. */
+	this.name = name;
+	this.color = undefined;
 }
 
 var lFoot = new BodyPart("Left foot"),
-    rFoot = new BodyPart("Right foot"),
-    lHand = new BodyPart("Left hand"),
-    rHand = new BodyPart("Right hand");
+	rFoot = new BodyPart("Right foot"),
+	lHand = new BodyPart("Left hand"),
+	rHand = new BodyPart("Right hand");
 
 var bodyParts = [lFoot, rFoot, lHand, rHand];
 var colors = ["red", "yellow", "green", "blue"];
 
-function moveYourBody(bodyPart, color) {
-    if (bodyPart.color === color) {
-        // already on that color, so try again
-        nextAction();
-    } else {
-        // move it
-        bodyPart.color = color;
-        // write it on the page
-        document.getElementById("bodyPart").innerHTML = bodyPart.name;
-        document.getElementById("color").innerHTML = color;
-    }
+function moveBody(bodyPart, color) {
+	/**
+	 * Update a given bodyPart and the page to a given color. However, if the
+	 * bodyPart is already on that color, do not move it there again; rather,
+	 * try again.
+	 *
+	 * Arguments:
+	 * bodyPart - the bodyPart to move
+	 * color - the color to try to move it to
+	 **/
+
+	if (bodyPart.color === color) {
+		// already on that color, so try again
+		nextAction();
+	} else {
+		// move it
+		// update the object
+		bodyPart.color = color;
+		// write it on the page
+		document.getElementById("bodyPart").innerHTML = bodyPart.name;
+		document.getElementById("color").innerHTML = color;
+	}
 }
 
 function nextAction() {
-    var nextBodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)],
-        nextColor = colors[Math.floor(Math.random() * colors.length)];
+	/* Generate a random body part and color, and call 'moveBody' with them. */
+	var nextBodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)],
+		nextColor = colors[Math.floor(Math.random() * colors.length)];
 
-    moveYourBody(nextBodyPart, nextColor);
+	moveBody(nextBodyPart, nextColor);
 }
